9bb3a71d82aeba6476a2f55c9b4c5276d583f175
Eugene Berezin <berezin.nlp@gmail.com>
2024-11-13 17:35:42 -0800

    Initial Setup

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Eugene Berezin on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
